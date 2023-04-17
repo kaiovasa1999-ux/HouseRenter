5d7d1c57e42abe152dc358bb5343c5af2f713907
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-04-17 14:38:06 +0300

    added view

--- HouseRenter/Models/HouseAd/HouseAdCreateMode.cs
+++ HouseRenter/Models/HouseAd/HouseAdCreateMode.cs
@@ -7,21 +7,23 @@ namespace HouseRenter.Models.HouseAd
 
     public class HouseAdCreateMode
     {
-        public int Id { get; set; }
-
-        public Category Category { get; set; }
-
-        [Required]
-        [Display(Name ="Year Created")]
-        [Range(minYearCreate,maxYearCreate)]
-        public DateTime YearCreated { get; set; }
-
-        public decimal Price { get; set; }
-
         [Required]
         [MinLength(LocationMinLength)]
         [MaxLength(LocationMaxLength)]
         public string Location { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        [Required]
+        [Display(Name = "Year Created")]
+        [Range(minYearCreate, maxYearCreate)]
+        public DateTime YearCreated { get; set; }
+
+        [Required]
+        [Range(MinPrice,MaxPrice)]
+        public decimal Price { get; set; }
+
 
         [EnumDataType(typeof(HouseAdCreateMode))]
         public HeatingType Heating { get; set; }
