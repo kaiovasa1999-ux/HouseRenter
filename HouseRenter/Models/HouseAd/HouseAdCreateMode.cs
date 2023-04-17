df76a66311ddaf2ee65b9e9e90157f19676aea2a
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-04-17 13:49:56 +0300

    added input model

---
+++ HouseRenter/Models/HouseAd/HouseAdCreateMode.cs
@@ -0,0 +1,25 @@
+﻿using HouseRenter.Data.Entities;
+
+namespace HouseRenter.Models.HouseAd
+{
+    public class HouseAdCreateMode
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public int BrokerId { get; set; }
+
+        public Broker Broker { get; set; }
+
+        public DateTime YearCreated { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Location { get; set; }
+
+        public HeatingType Heating { get; set; }
+    }
+}
